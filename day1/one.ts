837c76072c9f792b1ae5358adaef7ab885dbd7be
matt0hara <matthew@appliedimagination.com>
2024-12-01 12:59:44 -0700

    Fix file reference

--- day1/one.ts
+++ day1/one.ts
@@ -1,7 +1,7 @@
 import { parseLists } from "./functions.ts"
 import { abs } from "https://deno.land/x/math@v1.1.0/abs.ts";
 
-const [list1, list2] = await parseLists('example_input.txt')
+const [list1, list2] = await parseLists('input.txt')
 
 let sum = 0
 for(let i = 0; i <= list1.length - 1; i++) {
